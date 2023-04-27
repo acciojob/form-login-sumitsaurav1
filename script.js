3a2bdd936c3b23dc63d7929876b9ed9e7bb28189
Sumit saurav <117914094+sumitsaurav1@users.noreply.github.com>
2023-04-27 18:39:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function getFormvalue() {
-    //Write your code here
-
+    //Write your code here 
+    let form = document.getElementById("form1")
+    //alert(form.children[0].value)
+	alert(form.children[1].value)
 }
